2cb9712004859c99d468aa3b1eb2d893659e4b76
MatyTan <matytan@bituniverse.org>
2023-06-01 09:23:26 +0800

    模板方法

--- ch10/paper.go
+++ ch10/paper.go
@@ -1,22 +1,20 @@
 package ch9
 
-type Answer interface {
+// immatation of template method
+// 模拟多态，go中没有继承，只能通过接口实现多态
+type PaperTest interface {
+	TestQuestion1()
+}
+type PaperAnswer interface {
 	Answer1() string
 	Answer2() string
 }
 
 type TestPaper struct {
-	ans Answer
 }
 
 func (this *TestPaper) TestQuestion1() {
-	println("TestQuestion1")
-	println(this.ans.Answer1())
-}
-
-func (this *TestPaper) TestQuestion2() {
-	println("TestQuestion2")
-	println(this.ans.Answer2())
+	println("杨过得到，后来给了郭靖，炼成倚天剑、屠龙刀的玄铁可能是[ ] a.球磨铸铁 b. 马口铁 c.高速合金钢 d.碳素纤维 ")
 }
 
 func (this *TestPaper) Answer1() string {
@@ -28,32 +26,35 @@ func (this *TestPaper) Answer2() string {
 }
 
 type TestPaperA struct {
-	TestPaper
+	base TestPaper
+}
+
+func (this *TestPaperA) TestQuestion1() {
+	this.base.TestQuestion1()
+	println(this.Answer1())
 }
 
 func (this *TestPaperA) Answer1() string {
-	return "1 A"
-}
-
-func (this *TestPaperA) Answer2() string {
-	return "2 B"
+	return "A"
 }
 
 type TestPaperB struct {
-	TestPaper
+	base TestPaper
+}
+
+func (this *TestPaperB) TestQuestion1() {
+	this.base.TestQuestion1()
+	println(this.Answer1())
 }
 
 func (this *TestPaperB) Answer1() string {
-	return "1 A"
-}
-
-func (this *TestPaperB) Answer2() string {
-	return "2 B"
+	return "B"
 }
 
 func PaperMain() {
-	testPaperA := &TestPaperA{}
-	testPaperA.ans = testPaperA
-	testPaperA.TestQuestion1()
-	testPaperA.TestQuestion2()
+	var p PaperTest = &TestPaperA{}
+	p.TestQuestion1()
+
+	p = &TestPaperB{}
+	p.TestQuestion1()
 }
